58c155d064cc6515aee2a718d550494b75e5a1f1
Harshith M <mharshith200@gmail.com>
2026-05-30 15:02:29 +0530

    add neon db

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,21 +1,22 @@
+import "dotenv/config";
 import { PrismaClient } from "@/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { Pool } from "pg";
 
 const globalForPrisma = globalThis as unknown as {
-    prisma: PrismaClient | undefined;
-    pool: Pool | undefined;
+  prisma: PrismaClient | undefined;
+  pool: Pool | undefined;
 };
 
 // Reuse the pool across hot-reloads in development
 const pool =
-    globalForPrisma.pool ??
-    new Pool({ connectionString: process.env.DATABASE_URL });
+  globalForPrisma.pool ??
+  new Pool({ connectionString: process.env.DATABASE_URL });
 
 export const prisma =
-    globalForPrisma.prisma ?? new PrismaClient({ adapter: new PrismaPg(pool) });
+  globalForPrisma.prisma ?? new PrismaClient({ adapter: new PrismaPg(pool) });
 
 if (process.env.NODE_ENV !== "production") {
-    globalForPrisma.pool = pool;
-    globalForPrisma.prisma = prisma;
+  globalForPrisma.pool = pool;
+  globalForPrisma.prisma = prisma;
 }
